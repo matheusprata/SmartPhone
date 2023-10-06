0aca649791a035ed04b0e2351685e602801ac61e
matheusprata <matheusprata.ti@gmail.com>
2023-10-05 23:53:05 -0300

    alteração da classe SmartPhone para realizar as ações das funcionalidades

--- src/aparelho/SmartPhone.java
+++ src/aparelho/SmartPhone.java
@@ -1,7 +1,24 @@
 package src.aparelho;
 
+import src.funcionalidade.Internet;
+import src.funcionalidade.Musica;
+import src.funcionalidade.Telefone;
+
 public class SmartPhone {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Telefone telefone = new Telefone();
+        telefone.ligar();
+        telefone.atender();
+        telefone.iniciarCorreioVoz();
+
+        Musica musica = new Musica();
+        musica.tocar();
+        musica.pausar();
+        musica.selecionar();
+
+        Internet internet = new Internet();
+        internet.exibirPagina();
+        internet.adicionarNovaAba();
+        internet.atualizarPagina();
     }
 }
