ae07bdc951bf755ed9c396cc144fdcab85de0197
matheusprata <matheusprata.ti@gmail.com>
2023-10-05 23:46:24 -0300

    implementando metodos na funcionalidade Telefone

--- src/funcionalidade/Telefone.java
+++ src/funcionalidade/Telefone.java
@@ -1,4 +1,13 @@
 package src.funcionalidade;
 
 public class Telefone {
+    public void ligar(){
+        System.out.println("FAZENDO CHAMADA");
+    }
+    public void atender(){
+        System.out.println("ATENDENDO CHAMADA");
+    }
+    public void iniciarCorreioVoz(){
+        System.out.println("INICIANDO CORREIO DE VOZ");
+    }
 }
